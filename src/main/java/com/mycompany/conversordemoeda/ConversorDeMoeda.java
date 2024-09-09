ab3363644796348728137373d536346d010bb545
Fabricio <fabricioalvesfragoso@gmail.com>
2024-09-09 20:03:14 -0300

    ConversorDeMoeda

---
+++ src/main/java/com/mycompany/conversordemoeda/ConversorDeMoeda.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.conversordemoeda;
+
+/**
+ *
+ * @author Fabrício
+ */
+public class ConversorDeMoeda {
+
+    public static void main(String[] args) {
+        Tela form = new Tela();    
+      form.setVisible(true);
+    }
+}
